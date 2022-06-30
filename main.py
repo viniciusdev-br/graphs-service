07296f2c7cfb37fce2d1c81caa52f2de1026aeae
Jonathas Andrade <jonathas.andrade5@gmail.com>
2022-06-29 22:18:27 -0300

    fix: adjust cors in fastapi

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from pydantic import BaseModel
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from typing import List
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def HomePage():
